d29372d97673143048458fc38f624d719925a521
Taras B <Taras.Buha@gmail.com>
2020-10-06 16:54:13 +0300

    Added logo, and fixed example

--- sample/ConsoleApp/Program.cs
+++ sample/ConsoleApp/Program.cs
@@ -43,35 +43,37 @@ namespace ConsoleApp
             var topics = pcIds.Queues.Select(id => new { name = $"v2.routing.queues.{id}.conversations.emails", type = typeof(QueueConversationChatEventTopicChatConversation) }).ToList();
             topics.AddRange(pcIds.Users.Select(id => new { name = $"v2.users.{id}.presence", type = typeof(PresenceEventUserPresence) }));
 
-            var genesys = new GenesysNotifications(genesysConfig, logger);
-
-            await genesys.StartAsync(topics.ToDictionary(t => t.name, t => t.type));
-
-            var emails = genesys.Streams.Domain.OfType<QueueConversationChatEventTopicChatConversation>()
-                .Subscribe(e =>
-                {
-                    Console.WriteLine("{0} {1}", e.Id, e.Participants.Count);
-                });
-
-            var presence = genesys.Streams.Domain.OfType<PresenceEventUserPresence>()
-                .Subscribe(p =>
-                {
-                    Console.WriteLine("Presence {0}", p?.PresenceDefinition?.SystemPresence);
-                });
-
-            genesys.Streams.Pong.Subscribe(_ =>
+            using (var genesys = new GenesysNotifications(genesysConfig, logger))
             {
-                Console.WriteLine("Pong");
-            });
-            genesys.Streams.Heartbeats.Subscribe(_ =>
-            {
-                Console.WriteLine("Heart beat");
+                await genesys.StartAsync(topics.ToDictionary(t => t.name, t => t.type));
+
+                var emails = genesys.Streams.Domain.OfType<QueueConversationChatEventTopicChatConversation>()
+                    .Subscribe(e =>
+                    {
+                        Console.WriteLine("{0} {1}", e.Id, e.Participants.Count);
+                    });
+
+                var presence = genesys.Streams.Domain.OfType<PresenceEventUserPresence>()
+                    .Subscribe(p =>
+                    {
+                        Console.WriteLine("Presence {0}", p?.PresenceDefinition?.SystemPresence);
+                    });
+
+                genesys.Streams.Domain.Subscribe(obj =>
+                {
+                    Console.WriteLine(obj.ToString());
+                });
+                genesys.Streams.Pong.Subscribe(_ =>
+                {
+                    Console.WriteLine("Pong");
+                });
+                genesys.Streams.Heartbeats.Subscribe(_ =>
+                {
+                    Console.WriteLine("Heart beat");
+                });
+                genesys.Streams.SocketClosing.Subscribe(_ => Console.WriteLine("Socket closing"));
                 genesys.Ping();
-            });
-            genesys.Streams.SocketClosing.Subscribe(_ => Console.WriteLine("Socket closing"));
-
-            genesys.Ping();
-            genesys.Ping();
+            }
 
             Console.ReadLine();
 
